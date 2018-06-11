0eb5279affeff64a2318e8c5397caf28a994b5a9
alse <alseambusher@gmail.com>
2018-06-11 12:54:23 -0700

    Adding config file based off maskrcnn default

---
+++ waldo_config.py
@@ -0,0 +1,13 @@
+import os
+from Mask_RCNN.mrcnn.config import Config
+
+
+class Waldoconfig(Config):
+    NAME = "waldo"
+    IMAGES_PER_GPU = 2
+    NUM_CLASSES = 2
+    STEPS_PER_EPOCH = 100
+    DETECTION_MIN_CONFIDENCE = 0.9
+    COCO_WEIGHTS_PATH = os.path.join("models", "mask_rcnn_coco.h5")
+    MODEL_DIR = os.path.join("models", "logs")
+    DATA_DIR = "data"
