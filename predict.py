2224a6da54b28a4e19250c580124918576ebb83b
alse <alseambusher@gmail.com>
2018-06-12 16:54:16 -0700

    predict code

--- predict.py
+++ predict.py
@@ -1,6 +1,35 @@
-import os
 import sys
 sys.path.append("Mask_RCNN")
 
 from mrcnn.model import MaskRCNN
-from mrcnn import utils
+from waldo_config import Waldoconfig
+import numpy as np
+import skimage.draw
+from skimage.viewer import ImageViewer
+
+if __name__ == '__main__':
+
+    config = Waldoconfig(predict=True)
+    config.display()
+
+    model = MaskRCNN(mode="inference", config=config,
+                     model_dir=config.MODEL_DIR)
+
+    weights_path = model.find_last()[1]
+    print("weights_path: ", weights_path)
+    model.load_weights(weights_path, by_name=True)
+
+    image = skimage.io.imread(sys.argv[1])
+    masks = model.detect([image], verbose=1)[0]["masks"]
+
+    print("Masks:", masks)
+
+    gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 255
+    mask_filter = (np.sum(masks, -1, keepdims=True) >= 1)
+
+    if mask_filter.shape[0] > 0:
+        waldo = np.where(mask_filter, image, gray).astype(np.uint8)
+        viewer = ImageViewer(waldo)
+        viewer.show()
+    else:
+        print("Can't find Waldo. Hmm..")
